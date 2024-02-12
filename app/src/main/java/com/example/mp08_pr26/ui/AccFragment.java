0a0edac6da4bdf0f03a8624b7afbeefbed627a28
pablo <pablomejiast@gmail.com>
2024-02-12 17:41:48 +0100

    finished

--- app/src/main/java/com/example/mp08_pr26/ui/AccFragment.java
+++ app/src/main/java/com/example/mp08_pr26/ui/AccFragment.java
@@ -2,6 +2,11 @@ package com.example.mp08_pr26.ui;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,21 +16,61 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mp08_pr26.R;
 
 public class AccFragment extends Fragment {
 
     private AccViewModel mViewModel;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    SensorEventListener sensorListener;
 
     public static AccFragment newInstance() {
         return new AccFragment();
     }
 
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_acc, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_acc, container, false);
+        TextView textX = root.findViewById(R.id.textViewX);
+        TextView textY = root.findViewById(R.id.textViewY);
+        TextView textZ = root.findViewById(R.id.textViewZ);
+        sensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                // Valors de l'acceleròmetre en m/s^2
+                float xAcc = sensorEvent.values[0];
+                float yAcc = sensorEvent.values[1];
+                float zAcc = sensorEvent.values[2];
+
+                // Processament o visualització de dades...
+                textX.setText("X: " + xAcc);
+                textY.setText("Y: " + yAcc);
+                textZ.setText("Z: " + zAcc);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+                // Es pot ignorar aquesta CB de moment
+            }
+        };
+
+        // Seleccionem el tipus de sensor (veure doc oficial)
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // registrem el Listener per capturar els events del sensor
+        if( sensor!=null ) {
+            sensorManager.registerListener(sensorListener,sensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        return root;
     }
 
     @Override
